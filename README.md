44d8292bb1b32665a6131a3ccd32cc4078779d7d
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-04-07 13:23:05 +0000

    Adding of react and Tailwind

---
+++ README.md
@@ -0,0 +1,6 @@
+# Landing Page
+
+
+
+This Serve as landing page for the bus booking page for the bus booking system.
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className=" text-center text-3xl "> Hello Bus booking </h1>
+    </>
+  )
+}
+
+export default App
